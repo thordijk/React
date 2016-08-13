8e74a1f76cd20bf57106b5401358a0f7a5788e28
Ton Hordijk <Ton Hordijk>
2016-08-13 14:51:34 +0200

    Changed the DOM creation from react to JSX

--- js/components/forum.js
+++ js/components/forum.js
@@ -1,5 +1,27 @@
 var forum = React.createClass({
-   render: function(){
-       return React.createElement('div', null, React.createElement(forumHeader, null));
-   } 
+    getInitialState: function () {
+        return {
+            'allAnswers': {
+                '1': {
+                    body: 'Antwoord 1',
+                    correct: false
+                },
+                '2': {
+                    body: 'Antwoord 2',
+                    correct: false
+                },
+                '3': {
+                    body: 'Antwoord 3',
+                    correct: false
+                }
+            }
+        };
+    },
+    render: function () {
+        return (
+            <div>
+                <forumHeader allAnswers={this.state.allAnswers} />
+            </div>
+        );
+    }
 });
--- js/components/forumHeader.js
+++ js/components/forumHeader.js
@@ -1,9 +1,15 @@
-var containerDiv = React.createElement('div', {className: 'container-fluid'});
-var navbarHeaderDiv = React.createElement('div', {className: 'navbar-header'});
-var navbarBrandLink = React.createElement('a', {className: 'navbar-brand', href: '#'}, 'React Forum');
-
 var forumHeader = React.createClass({
    render: function(){
-       return React.createElement('nav', { className: 'navbar navbar-default' }, containerDiv, navbarHeaderDiv, navbarBrandLink);
+       return (
+           <nav className="navbar navbar-default">
+               <div className="container-fluid">
+                   <div className="navbar-header">
+                       <a className="navbar-brand" href="#">
+                           React forum
+                        </a>
+                   </div>
+               </div>
+           </nav>
+       );
    } 
 });
